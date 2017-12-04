9415aab9d04d9000409ce4f63226504dc76c03b8
Michael Graupner <michael.graupner@parisdescartes.fr>
2017-12-04 22:51:47 +0100

    added new figure showing wrongly classified numbers in test set

--- plotResults.py
+++ plotResults.py
@@ -150,3 +150,89 @@ fname = 'fig_perceptron_progress'
 
 plt.savefig(fname+'.png')
 plt.savefig(fname+'.pdf')
+
+plt.clf()
+
+## new figure ############################################################
+
+
+d = pickle.load(open('numbersMissclassified.p'))
+
+fig_width = 6 # width in inches
+fig_height = 5  # height in inches
+fig_size =  [fig_width,fig_height]
+params = {'axes.labelsize': 14,
+          'axes.titlesize': 13,
+          'font.size': 11,
+          'xtick.labelsize': 11,
+          'ytick.labelsize': 11,
+          'figure.figsize': fig_size,
+          'savefig.dpi' : 600,
+          'axes.linewidth' : 1.3,
+          'ytick.major.size' : 4,      # major tick size in points
+          'xtick.major.size' : 4      # major tick size in points
+          #'edgecolor' : None
+          #'xtick.major.size' : 2,
+          #'ytick.major.size' : 2,
+          }
+rcParams.update(params)
+
+# set sans-serif font to Arial
+rcParams['font.sans-serif'] = 'Arial'
+
+# create figure instance
+fig = plt.figure()
+
+
+# define sub-panel grid and possibly width and height ratios
+gs = gridspec.GridSpec(1, 1,
+                       #width_ratios=[1,1.2],
+                       #height_ratios=[1,1]
+                       )
+
+# define vertical and horizontal spacing between panels
+gs.update(wspace=0.3,hspace=0.4)
+
+# possibly change outer margins of the figure
+plt.subplots_adjust(left=0.15, right=0.95, top=0.92, bottom=0.15)
+
+# sub-panel enumerations
+#plt.figtext(0.06, 0.92, 'A',clip_on=False,color='black', weight='bold',size=22)
+#plt.figtext(0.47, 0.92, 'B',clip_on=False,color='black', weight='bold',size=22)
+#plt.figtext(0.06, 0.47, 'C',clip_on=False,color='black', weight='bold',size=22)
+#plt.figtext(0.47, 0.47, 'D',clip_on=False,color='black', weight='bold',size=22)
+
+
+# first sub-plot #######################################################
+ax0 = plt.subplot(gs[0])
+
+# title
+ax0.set_title('Misclassified numbers in test set')
+
+# diplay of data
+#ax0.axhline(y=0,ls='--',color='0.5',lw=2)
+#ax0.axvline(x=0,ls='--',color='0.5',lw=2)
+ax0.hist(d,bins=np.arange(-0.5,10.5,1),align='mid',rwidth=0.5)
+
+#ax0.plot(x,cosy,label='cos')
+
+# removes upper and right axes
+# and moves left and bottom axes away
+ax0.spines['top'].set_visible(False)
+ax0.spines['right'].set_visible(False)
+ax0.spines['bottom'].set_position(('outward', 10))
+ax0.spines['left'].set_position(('outward', 10))
+ax0.yaxis.set_ticks_position('left')
+ax0.xaxis.set_ticks_position('bottom')
+
+# legends and labels
+#plt.legend(loc=1,frameon=False)
+
+plt.xlabel('number')
+plt.ylabel('mis-classifications')
+
+
+fname = 'fig_test_classifications'
+
+plt.savefig(fname+'.png')
+plt.savefig(fname+'.pdf')
